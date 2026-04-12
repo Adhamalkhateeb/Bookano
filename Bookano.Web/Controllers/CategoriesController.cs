fe896a034edf7cce2180289e2cbac98fa506009f
Adhamalkhateeb <adhamalkhateeb2006@gmail.com>
2026-04-13 00:04:14 +0200

    - Added Categories Controller - Added Categories Index Veiw

---
+++ Bookano.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Bookano.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // TODO: Using ViewModels instead of passing entities directly to the view
+            var categories = _context.Categories.AsNoTracking().ToList();
+            return View(categories);
+        }
+    }
+}
